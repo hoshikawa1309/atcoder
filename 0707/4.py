e8cca42824d715f2af6c3cb11667ba41ce681fc5
hosokawa1309 <15rd167@gmail.com>
2019-09-28 16:33:45 +0900

    add 0707/4.py

--- 0707/4.py
+++ 0707/4.py
@@ -1,3 +1,28 @@
+import numpy as np
 N = int(input())
 A = list(map(int , input().split()))
-print(A)
+'''
+#print(A)
+b = [sum(A) - 2 * sum(A[1::2])]
+print(b)
+W = [0] * N
+tmp0 = int(np.ceil(A[N-1] / 2) % 2)
+tmp1 = int(np.ceil(A[0] / 2) % 2)
+if tmp0 == 0 and tmp1 == 0:
+    W[0] = int(min(np.ceil(A[N-1] / 2),np.ceil(A[0] / 2)))
+elif tmp0 == 0 and tmp1 == 1:
+    W[0] = int(np.ceil(A[N-1] / 2))
+elif tmp0 == 1 and tmp1 == 0:
+    W[0] = int(np.ceil(A[0] / 2))
+else:
+    W[0] = 0
+for i in range(1,N):
+    W[i] = 2 * A[i - 1] - W[i - 1]
+print(*W)
+'''
+W = [0] * N
+W[0] = sum(A) - 2 * sum(A[1:N - 1:2])
+
+for i in range(1,N):
+    W[i] = 2 * A[i - 1] - W[i - 1]
+print(*W)
