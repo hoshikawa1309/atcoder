730ce1d238f914e85ad3a3c58c2ebbdd6afd7273
hosokawa1309 <15rd167@gmail.com>
2019-10-04 12:39:49 +0900

    add 0527/4.py

---
+++ ABC/0526/4.py
@@ -0,0 +1,31 @@
+from collections import deque
+from copy import deepcopy
+N, K = map(int, input().split())
+#V = list(map(int, input().split()))
+V = deque(map(int, input().split()))
+ans = 0
+for l in range(K):
+    for r in range(0 , K - l):
+        work_V = deepcopy(V)
+        if l + r > N:
+            continue
+        d = K - (r + l)
+        now = 0
+        hold = []
+        for i in range(l):
+            #hold.append(V[i])
+            hold.append(work_V.popleft())
+        for i in range(1 , r + 1):
+            #hold.append(V[-i])
+            hold.append(work_V.pop())
+        hold.sort()
+        now = sum(hold)
+        for i in range(d):
+            if i >= len(hold):
+                break
+            if hold[i] > 0:
+                break
+            else:
+                now -= hold[i]
+        ans = max(ans , now)
+print(ans)
