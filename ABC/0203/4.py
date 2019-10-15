f3d45c4202eb58817a54f4a74b6de37d6871e35e
hosokawa1309 <15rd167@gmail.com>
2019-10-15 13:01:02 +0900

    add 0203/4.py

--- ABC/0203/4.py
+++ ABC/0203/4.py
@@ -1,7 +1,10 @@
+import numpy as np
+import math
 N , K = map(int,input().split())
 A = list(map(int,input().split()))
+'''
+A.sort()
 max = 0
-
 
 for i in range(K + 1):
     sum = 0
@@ -10,3 +13,24 @@ for i in range(K + 1):
         if sum > max:
             max = sum
 print(max)
+'''
+count1_list = np.zeros(40)
+count0_list = np.zeros(40)
+for a in A:
+    binary = bin(a)
+    binary = binary[::-1]
+    for i in range(len(binary) - 2):
+        if binary[i] == "1":
+            count1_list[i] += 1
+        else:
+            count0_list[i] += 1
+
+ans = []
+for i in range(40):
+    if count1_list[i] < count0_list[i]:
+        ans.append(1)
+    else:
+        ans.append(0)
+print(ans)
+
+
