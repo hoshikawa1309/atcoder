4cddd88cbb02bdb4ae5fb773d04d3b3a2fb3451e
hosokawa1309 <15rd167@gmail.com>
2019-10-18 11:40:22 +0900

    fix digit_dp

--- study/greedy/DP/digit_dp.py
+++ study/greedy/DP/digit_dp.py
@@ -1,15 +1,26 @@
 D = int(input())
 N = input()
+#dp = [10000][2][100]
+mod = 10 ** 9 + 1
 
-def rec():
-    pass
+def rec(k,tight,sum_val):
+    if k == len(N):
+        if sum_val % D == 0 and sum_val != 0:
+            return 1
+        else:
+            return 0
+    x = int(N[k])
+    if tight == 1:
+        r = x
+    else:
+        r = 10
+    res = 0
+    for i in range(r):
+        res += rec(k + 1 ,tight and i == r, sum_val + i )
+        res %= mod
+
+    return res
 
 
-'''
-N = int(input())
-cnt = 0
-for i in range(1 , N + 1):
-    if i % D == 0:
-        cnt += 1
-print(cnt)
-'''
+
+print(rec(0 , True , 0))
