394a85bc81a16dd3e642fea393d7696186fe91cb
hosokawa1309 <15rd167@gmail.com>
2019-10-22 14:49:00 +0900

    fix all_green.py

--- study/ant/2/2-1/all_green.py
+++ study/ant/2/2-1/all_green.py
@@ -2,16 +2,28 @@ D , G = map(int,input().split())
 questions = []
 for i in range(D):
     p , c = map(int,input().split())
-    tmp = []
-    for j in range(p):
-        if j == p - 1:
-            tmp.extend([c])
-        else:
-            tmp.extend([(i + 1) * 100])
-    questions.append(tmp)
-questions = [for x in zip(*questions)]
-print(questions)
-ans = 0
+    questions.append([p,c])
 
-
-print(solve(0,0))
+ans = float("inf")
+for bit in range(2 ** D):
+    tmp_ans = 0
+    tmp_sum = 0
+    for j in range(D):
+        if bit >> j & 1:
+            tmp_sum += questions[j][0] * 100 * (j + 1) + questions[j][1]
+            tmp_ans += questions[j][0]
+    if tmp_sum < G:
+        for j in range(D - 1 , -1 , -1):
+            if bit >> j & 1:
+                continue
+            else:
+                for k in range(questions[j][0]):
+                    tmp_ans += 1
+                    tmp_sum += 100 * (j + 1)
+                    if tmp_sum >= G:
+                        break
+                if tmp_sum >= G:
+                    break
+    if tmp_ans < ans and tmp_sum >= G:
+        ans = tmp_ans
+print(ans)
