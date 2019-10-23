67fbec33b6953f9daa4084b43500a1d50ca6feba
hosokawa1309 <15rd167@gmail.com>
2019-10-23 10:04:44 +0900

    add dfs.py

---
+++ study/ant/2_basic/2-1_FullSearch/2-1-2_/dfs.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+H , W = map(int,input().split())
+town = []
+for i in range(H):
+    wide = list(input())
+    for j in range(W):
+        if wide[j] == "s":
+            start_x = j
+            start_y = i
+    town.append(wide)
+
+dx = [0,1,0,-1]
+dy = [-1,0,1,0]
+def dfs(x , y , flag):
+    visited.append([x, y])
+    if town[y][x] == "g":
+        flag = True
+    else:
+        for i in range(4):
+            next_x = x + dx[i]
+            next_y = y + dy[i]
+            if 0 <= next_x and next_x < W and 0 <= next_y and next_y < H and town[next_y][next_x] != "#" and not [next_x,next_y] in visited:
+                flag = dfs(next_x,next_y , flag)
+            else:
+                continue
+    return flag
+
+visited = []
+if dfs(start_x,start_y , False):
+    print("Yes")
+else:
+    print("No")
