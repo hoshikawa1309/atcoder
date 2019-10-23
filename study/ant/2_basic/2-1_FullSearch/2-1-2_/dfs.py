d9817c6825e9bfb847b3036e6962d07a4e222348
hosokawa1309 <15rd167@gmail.com>
2019-10-23 13:20:47 +0900

    fix dfs.py

--- study/ant/2_basic/2-1_FullSearch/2-1-2_/dfs.py
+++ study/ant/2_basic/2-1_FullSearch/2-1-2_/dfs.py
@@ -1,44 +1,33 @@
+
 def main():
     import sys
-    sys.setrecursionlimit(10 ** 6)
-    import sys
-    def input():
-        return sys.stdin.readline()[:-1]
-
+    sys.setrecursionlimit(4100000)
     H , W = map(int,input().split())
-    town = []
-    start_x ,start_y = -1 , -1
-    for i in range(H):
-        wide = list(input())
-        if start_y == -1 and start_x == -1:
-            for j in range(W):
-                if wide[j] == "s":
-                    start_x = j
-                    start_y = i
-        town.append(wide)
-
-    dx = [0,1,0,-1]
-    dy = [-1,0,1,0]
-    def dfs(x , y , flag , visited):
-        if flag:
-            return True
-        visited.append([x, y])
-        if town[y][x] == "g":
-            flag = True
-        else:
-            for i in range(4):
-                next_x = x + dx[i]
-                next_y = y + dy[i]
-                if 0 <= next_x and next_x < W and 0 <= next_y and next_y < H and town[next_y][next_x] != "#" and not [next_x,next_y] in visited:
-                    flag = dfs(next_x,next_y , flag , visited)
-                else:
-                    continue
-        return flag
-
-    #visited = []
-    if dfs(start_x,start_y , False,[]):
-        print("Yes")
-    else:
-        print("No")
-
+    town = [input() for _ in range(H)]
+    for row in range(H):
+        for column in range(W):
+            if town[row][column] == "s":
+                start_column , start_row = column , row
+            if town[row][column] == "g":
+                goal_column , goal_row = column , row
+    visit_possible = [[False for _ in range(W)] for _ in range(H)]
+    dx = [0, 1, 0, -1]
+    dy = [-1, 0, 1, 0]
+    def dfs(t,r , c):
+        if 0 > c or c >= W or 0 > r or r >= H or t[r][c] == "#":
+            return
+        if visit_possible[r][c]:
+            return
+        visit_possible[r][c] = True
+        for i in range(4):
+            dfs(t,r + dx[i],c + dy[i])
+    dfs(town,start_row, start_column)
+    print("Yes" if visit_possible[goal_row][goal_column] else "No")
 main()
+'''
+4 5
+s####
+....#
+#####
+#...g
+'''
