71048bf8196b652f60f89010961da2f3ac23c988
hosokawa1309 <15rd167@gmail.com>
2019-10-24 12:26:07 +0900

    add bfs.py

---
+++ study/ant/2_basic/2-1_FullSearch/2-1-3_BFS/bfs.py
@@ -0,0 +1,10 @@
+R , C = map(int,input().split())
+sy , sx = map(int,input().split())
+gy , gx = map(int,input().split())
+
+maze = [[input().split()] for _ in range(R)]
+
+def dfs():
+    pass
+
+dfs()
