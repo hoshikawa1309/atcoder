dc763d5174aaf851c1e25180cdf09d468ba2a557
hosokawa1309 <15rd167@gmail.com>
2019-10-26 11:23:55 +0900

    refactor change.py

---
+++ study/ant/2_basic/2-1_FullSearch/2-1-4_enamurate/card_game.py
@@ -0,0 +1,9 @@
+import itertools
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+victory = 0
+for a , b in itertools.product(A,B):
+
+
+print(victory /  // N))
--- study/ant/2_basic/2-2/2-2-1_coin/change.py
+++ study/ant/2_basic/2-2/2-2-1_coin/change.py
@@ -1,6 +1,13 @@
 N = int(input())
 N = 1000 - N
 count = 0
+coins = [500,100,50,10,5,1]
+for c in coins:
+    count += N // c
+    N %= c
+print(count)
+'''
+
 while N >= 500:
     N = N - 500
     count += 1
@@ -24,3 +31,4 @@ while N >= 1:
     N = N - 1
     count += 1
 print(count)
+'''
