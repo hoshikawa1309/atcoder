536aa7cd1b0fc2cd06ef60f14a77e767ca31f3ad
hosokawa1309 <15rd167@gmail.com>
2019-10-26 09:12:22 +0900

    add cup.py

---
+++ study/ant/2_basic/2-1_FullSearch/2-1-3_BFS/cup.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+def recursion(start,work,end):
+    pass
+
+n_list = []
+m_list = []
+a_list = []
+b_list = []
+c_list = []
+while True:
+    n , m = map(int,input().split())
+    if n == m == 0:
+        break
+    n_list.append(n)
+    m_list.append(m)
+    a = list(input().split())
+    a_list.append(a)
+    b = list(input().split())
+    b_list.append(b)
+    c = list(input().split())
+    c_list.append(c)
+
+for i in range(len(n_list)):
+    
