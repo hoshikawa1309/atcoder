6be62ebf6a3f583964bad602b4a53f80a3c9dd92
hosokawa1309 <15rd167@gmail.com>
2019-10-26 15:16:44 +0900

    fix card_game.py

--- study/ant/2_basic/2-1_FullSearch/2-1-4_enamurate/card_game.py
+++ study/ant/2_basic/2-1_FullSearch/2-1-4_enamurate/card_game.py
@@ -3,18 +3,19 @@ import math
 N = int(input())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
-victory = 0
-for a in A:
-    b_tmp = B.pop(0)
-    B.append(b_tmp)
-    a_w = 0
-    b_w = 0
-    for b in B:
-        if a > b:
-            a_w += 1
-        else:
-            b_w += 1
-    if a_w > b_w:
-        victory += 1
+win_cnt = 0
+for a in itertools.permutations(A , N):
+    for b in itertools.permutations(B , N):
+        a_victory = 0
+        b_victory = 0
+        for i in range(N):
+            if a[i] > b[i]:
+                a_victory += 1
+            elif a[i] < b[i]:
+                b_victory += 1
+        if a_victory > b_victory:
+            win_cnt += 1
 
-print(victory / (len(list(itertools.product(A,B))) // N))
+
+
+print(win_cnt / math.factorial(N) ** 2)
