75e2140ede222f206f1d5c8b66b88fd8dcdd5d51
hosokawa1309 <15rd167@gmail.com>
2019-10-26 14:23:28 +0900

    fix cardgame.py

--- study/ant/2_basic/2-1_FullSearch/2-1-4_enamurate/card_game.py
+++ study/ant/2_basic/2-1_FullSearch/2-1-4_enamurate/card_game.py
@@ -1,9 +1,20 @@
 import itertools
+import math
 N = int(input())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 victory = 0
-for a , b in itertools.product(A,B):
+for a in A:
+    b_tmp = B.pop(0)
+    B.append(b_tmp)
+    a_w = 0
+    b_w = 0
+    for b in B:
+        if a > b:
+            a_w += 1
+        else:
+            b_w += 1
+    if a_w > b_w:
+        victory += 1
 
-
-print(victory /  // N))
+print(victory / (len(list(itertools.product(A,B))) // N))
