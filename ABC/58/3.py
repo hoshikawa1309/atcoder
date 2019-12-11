a42fcfd7ecf26a384ae6b625e4f7bfb53c8635c4
hosokawa1309 <15rd167@gmail.com>
2019-12-11 13:35:50 +0900

    add 58

--- ABC/58/3.py
+++ ABC/58/3.py
@@ -5,12 +5,23 @@ for _ in range(N):
     s = list(input())
     s.sort()
     S.append(s)
-S = Counter(S)
-S_key =
-char_num = ord("a")
-for i in range(26):
-    flag = True
-    for j in range(N):
-        for key , val in S[j]:
-            if key == chr(char_num + i):
+
+work = S[0]
+for i in range(1 , N):
+    work_lst = []
+    while len(work) != 0 and len(S[i]) != 0:
+        if work[0] == S[i][0]:
+            c = S[i].pop(0)
+            c = work.pop(0)
+            work_lst.append(c)
+        elif work[0] > S[i][0]:
+            S[i].pop(0)
+        elif work[0] < S[i][0]:
+            work.pop(0)
+    work = work_lst
+if N == 1:
+    print("".join(s))
+else:
+    print("".join(work_lst))
+
 
