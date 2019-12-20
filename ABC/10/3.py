9a24048d7f4d6c1f9def56d0656ee4e8e9a16029
hosokawa1309 <15rd167@gmail.com>
2019-12-20 15:07:37 +0900

    add 10

--- ABC/10/3.py
+++ ABC/10/3.py
@@ -2,15 +2,16 @@ import math
 txa ,tya, txb , tyb, T , V = map(int,input().split())
 n = int(input())
 girls = [list(map(int,input().split())) for _ in range(n)]
+Flag =False
 now_x , now_y = txa , tya
 distance = 0
 for i in range(n):
-    distance += math.sqrt((girls[i][0] - now_x ) ** 2 + (girls[i][1] - now_y) ** 2 )
-    now_x , now_y = girls[i][0] , girls[i][1]
-distance += math.sqrt((now_x - txb) ** 2 + (now_y - tyb) ** 2 )
-print(distance)
-print(T * V)
-if distance <= T * V:
+    distance = math.sqrt((girls[i][0] - txa) ** 2 + (girls[i][1] - tya) ** 2 )
+    distance += math.sqrt((girls[i][0] - txb) ** 2 + (girls[i][1] - tyb) ** 2 )
+    if distance <= T * V:
+        Flag = True
+        break
+if Flag:
     print("YES")
 else:
     print("NO")
