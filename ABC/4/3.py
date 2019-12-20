8288632179973d14a81adfa4392fb6253e2f08fa
hosokawa1309 <15rd167@gmail.com>
2019-12-20 15:58:25 +0900

    add 26

--- ABC/4/3.py
+++ ABC/4/3.py
@@ -1,9 +1,7 @@
 N = int(input())
-N = N % 6
-add_num = 2
-for i in range(6):
-    if i == N:
-        print("1" , end = "")
-        add_num = 1
-    else:
-        print(i + add_num, end = "")
+N = N % 30
+num = ["1","2","3","4","5","6"]
+for i in range(N):
+    idx = i % 5
+    num[idx] , num[idx + 1] = num[idx + 1] , num[idx]
+print("".join(num))
