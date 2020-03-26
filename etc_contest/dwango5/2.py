40ae9e05ef7320a3a1d6f5c42a2ca6baa3f23114
hosokawa1309 <15rd167@gmail.com>
2020-03-26 09:30:30 +0900

    fix dwango5/2

--- etc_contest/dwango5/2.py
+++ etc_contest/dwango5/2.py
@@ -1,10 +1,19 @@
 N, K = map(int, input().split())
-a = list(map(int, input().split()))
-start = 0
+A = list(map(int, input().split()))
 candidates = []
-for i in range(start, N - 1):
-    now = [a[start]]
-    for j in range(i + 1, N):
-        candidates.append(now)
-        now.append(a[j])
-print(candidates)
+for i, _ in enumerate(A):
+    add_num = 0
+    for a in A[i:]:
+        add_num += a
+        candidates.append(add_num)
+
+ans = 0
+for i in range(40, -1, -1):
+    k_candidates = []
+    for candidate in candidates:
+        if candidate & (1 << i):
+            k_candidates.append(candidate)
+    if len(k_candidates) >= K:
+        ans += 1 << i
+        candidates = k_candidates
+print(ans)
