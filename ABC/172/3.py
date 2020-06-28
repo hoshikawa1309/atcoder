047e0826aea83be721f711df357c6341403675cc
hosokawa1309 <15rd167@gmail.com>
2020-06-28 09:40:42 +0900

    fix abc172/3.py abc172/3_2.py

--- ABC/172/3.py
+++ ABC/172/3.py
@@ -3,14 +3,15 @@ N, M, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 ans = 0
-A_cumulative_sum = [A[0]]
+A_cumulative_sum = [0]
 B_cumulative_sum = [B[0]]
-for i in range(1, N):
-    A_cumulative_sum.append(A[i] + A_cumulative_sum[i - 1])
+for i in range(N):
+    A_cumulative_sum.append(A[i] + A_cumulative_sum[i])
 for i in range(1, M):
     B_cumulative_sum.append(B[i] + B_cumulative_sum[i - 1])
 
-for i in range(N):
+for i in range(N + 1):
+
     if K < A_cumulative_sum[i]:
         remaining_time = K
         idx = bisect.bisect_right(B_cumulative_sum, remaining_time)
@@ -19,5 +20,7 @@ for i in range(N):
     else:
         remaining_time = K - A_cumulative_sum[i]
         idx = bisect.bisect_right(B_cumulative_sum, remaining_time)
-        ans = max(ans, i + 1 + idx)
+        ans = max(ans, i + idx)
 print(ans)
+
+# 113897
--- ABC/172/3_2.py
+++ ABC/172/3_2.py
@@ -1,6 +1,15 @@
+'''
 N, M, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
+'''
+with open('./b09.txt') as f:
+    N, M, K = map(int, f.readline().split())
+    A = list(map(int, f.readline().split()))
+    B = list(map(int, f.readline().split()))
+
+A_cnt = 0
+B_cnt = 0
 ans = 0
 nowK = K
 for A_idx in range(N):
@@ -8,24 +17,24 @@ for A_idx in range(N):
         break
     ans += 1
     nowK -= A[A_idx]
+    A_cnt += 1
 for B_idx in range(M):
     if nowK < B[B_idx]:
         break
     ans += 1
     nowK -= B[B_idx]
+    B_cnt += 1
 tmp_ans = ans
 finish = False
-for i in range(A_idx - 1, -1, -1):
-    nowK += A[i]
+for i in range(A_cnt, 0, -1):
+    nowK += A[i - 1]
     tmp_ans -= 1
-    for j in range(B_idx, M):
+    for j in range(B_cnt, M):
         if finish or nowK < B[j]:
             break
         nowK -= B[j]
         tmp_ans += 1
-        if j == M - 1:
-            finish = True
-    B_idx = j
+        B_cnt += 1
     ans = max(tmp_ans, ans)
 print(ans)
 
