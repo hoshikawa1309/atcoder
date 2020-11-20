d4701dd758a20195baf336ed752704564d11b1e1
hosokawa1309 <15rd167@gmail.com>
2020-11-20 10:59:10 +0900

    add arc107/2.py

--- ABC/70/4.py
+++ ABC/70/4.py
@@ -1,12 +1,30 @@
-import math
-
-
-def lcm(a, b):
-    return a * b // math.gcd(a, b)
+def dijkstra(start, node_cnt, now_graph):
+    # start:始点、node_cnt:ノード数、now_graph:隣接グラフ
+    import heapq
+    INF = float('inf')
+    distance = [INF] * node_cnt
+    distance[start] = 0
+    hq = [[0, start]]
+    heapq.heapify(hq)
+    seen = [False] * node_cnt
+    while hq:
+        _, now = heapq.heappop(hq)
+        seen[now] = True
+        for to, cost in now_graph[now]:
+            if not seen[to] and distance[now] + cost < distance[to]:
+                distance[to] = distance[now] + cost
+                heapq.heappush(hq, list([distance[to], to]))
+    return distance
 
 
 N = int(input())
-ans = int(input())
+graph =[[] for _ in range(N)]
 for _ in range(N - 1):
-    ans = lcm(ans, int(input()))
-print(ans)
+    a, b, c = map(int, input().split())
+    graph[a - 1].append((b - 1, c))
+    graph[b - 1].append((a - 1, c))
+Q, K = map(int, input().split())
+dist = dijkstra(K - 1, N, graph)
+for _ in range(Q):
+    x, y = map(int, input().split())
+    print(dist[x - 1] + dist[y - 1])
--- etc_contest/indeednow-quala/3.py
+++ etc_contest/indeednow-quala/3.py
@@ -1,18 +1,14 @@
 N = int(input())
-scores = [int(input()) for _ in range(N)]
+scores = []
+for _ in range(N):
+    x = int(input())
+    if x:
+        scores.append(x)
 scores.sort(reverse=True)
 Q = int(input())
-capacity = [int(input()) for _ in range(Q)]
-if len(set(scores)) == 1 and scores[0] == 0:
-    for _ in range(Q):
+for _ in range(Q):
+    capacity = int(input())
+    if capacity >= len(scores):
         print(0)
-    exit()
-for c in capacity:
-    if c == 0:
-        print(scores[0] + 1)
-    elif scores[c] == 0:
-        print(0)
-    elif scores[c - 1] == scores[c]:
-        print(scores[c - 1] + 1)
     else:
-        print(scores[c - 1])
+        print(scores[capacity] + 1)
