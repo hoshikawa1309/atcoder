0e91b198878dbb267608a9b33e6176973327d812
hosokawa1309 <15rd167@gmail.com>
2020-11-24 15:42:30 +0900

    fix agc013/1.py

--- AGC/13/1.py
+++ AGC/13/1.py
@@ -1,18 +1,22 @@
 N = int(input())
 A = list(map(int, input().split()))
-if N < 3:
-    print(1)
-    exit()
 ans = 1
-work = [A[0]]
-now = A[0]
-for i in range(1, N):
-    if now == A[i]:
-        continue
-    work.append(A[i])
-    now = A[i]
-A = work
-for i in range(len(A) - 2):
-    if A[i] < A[i + 1] > A[i + 2] or A[i] > A[i + 1] < A[i + 2]:
-        ans += 1
+flag = 0
+now = 0
+for i in range(N - 1):
+    now_A = A[i]
+    next_A = A[i + 1]
+    if now_A < next_A:
+        if flag == -1:
+            flag = 0
+            ans += 1
+        else:
+            flag = 1
+    elif now_A > next_A:
+        if flag == 1:
+            flag = 0
+            ans += 1
+        else:
+            flag = -1
+
 print(ans)
