5cb1f748275abcff3a19ae165c55b6c1d99d0390
hosokawa1309 <15rd167@gmail.com>
2020-11-26 14:17:22 +0900

    add arc107/3.py

--- ABC/179/4.py
+++ ABC/179/4.py
@@ -1,22 +1,22 @@
 N, K = map(int, input().split())
 jump = []
+MOD = 998244353
 for _ in range(K):
     l, r = map(int, input().split())
     jump.append([l, r])
 jump.sort()
-# print(jump)
-MOD = 998244353
-dp = [0] * (N + 1)
-dp[1] = 1
+dp = [0] * N
+dp[0] = 1
 dp_sum = [0] * (N + 1)
 dp_sum[1] = 1
-for i in range(2, N + 1):
+for i in range(1, N):
     for l, r in jump:
-        min_section = max(0, i - r - 1)
-        max_section = max(0, i - l)
-        dp[i] += (dp_sum[max_section] - dp_sum[min_section]) % MOD
+        max_val = max(0, i - l + 1)
+        min_val = max(0, i - r)
+        dp[i] += dp_sum[max_val] - dp_sum[min_val]
     dp[i] %= MOD
-    dp_sum[i] = (dp[i] + dp_sum[i - 1]) % MOD
-print(dp)
-print(dp_sum)
+    dp_sum[i + 1] = dp[i] + dp_sum[i]
+    dp_sum[i + 1] %= MOD
+# print(dp)
+# print(dp_sum)
 print(dp[-1])
